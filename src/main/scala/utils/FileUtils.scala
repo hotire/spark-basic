f6a9c09ada6c4d10f6f83bbf77ea130f5484955f
hotire <gngh0101@gmail.com>
2021-11-29 22:36:30 +0900

    Add delete

--- src/main/scala/utils/FileUtils.scala
+++ src/main/scala/utils/FileUtils.scala
@@ -11,4 +11,14 @@ object FileUtils {
     fs.rename(origin.getPath, new Path(dst))
   }
 
+  /**
+   * Directory 일 경우 파일이 존재할 경 삭제할 수 없다.
+   * recursive true 설정한 경우 재귀로 모든 file을 삭제하고 Directory 삭제한다.
+   *
+   */
+  def delete(spark: SparkSession, path: String, recursive: Boolean): Boolean = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    fs.delete(new Path(path), true)
+  }
+
 }
